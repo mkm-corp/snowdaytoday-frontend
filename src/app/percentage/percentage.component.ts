13ee128adf0c99bcef4b8901aef3781852d237b8
89apt89 <89apt89@protonmail.ch>
2019-02-24 23:10:34 -0500

    update

--- src/app/percentage/percentage.component.ts
+++ src/app/percentage/percentage.component.ts
@@ -13,14 +13,20 @@ import { fadeIn } from 'ng-animate';
 export class PercentageComponent implements OnInit {
   percentage: number
   error: string
+  date: Date
+  dateString: string
   constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) {
   }
   showValue(loc: string) {
     this.apiService.getValue(loc).subscribe((data: APIResponse) => {
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',' December']
       this.percentage = data.chance
       if (data.error) {
         this.error = data.error
       }
+      this.date = new Date(data.date)
+      this.dateString = days[this.date.getDay()]+", "+months[this.date.getMonth()]+" "+this.date.getDay()+1
     })
   }
   ngOnInit() { 
